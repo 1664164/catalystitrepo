8b6a8586310cca16201714c52a36a455544b844e
Massimo <massimo.giangrande01@gmail.com>
2021-01-13 01:52:43 -0500

    implemented the code to test if there is a connection with index.php file to the sql database and the code to create the users table

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "catalystit";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+
+//sql create table
+$sql = "CREATE TABLE users (
+name VARCHAR(50) NOT NULL,
+surname VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL UNIQUE,
+)";
+
+
 //Open the file.
 $fileHandle = fopen("users.csv", "r");
  
@@ -10,4 +30,7 @@ while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
 	echo $row[2] . '<br>';
 	echo '<br>';
 }
+//Close the file
+fclose($fileHandle);
+
 ?>
