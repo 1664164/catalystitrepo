96149a3a30d8bfea7bab4d4cae7c852d52bdc2bb
Massimo <massimo.giangrande01@gmail.com>
2021-01-12 23:03:49 -0500

    wrote the code in the index.php file so that it goes through users.csv file

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-
-echo "Hello World.";
-
+//Open the file.
+$fileHandle = fopen("users.csv", "r");
+ 
+//Loop through the CSV rows.
+while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
+    //Dump out the row for the sake of clarity.
+    echo $row[0] . '<br>';
+	echo $row[1] . '<br>';
+	echo $row[2] . '<br>';
+	echo '<br>';
+}
 ?>
