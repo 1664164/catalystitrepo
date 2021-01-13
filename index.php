f94cef877aa687857384b8665540675c94861c6c
Massimo <massimo.giangrande01@gmail.com>
2021-01-13 16:31:17 -0500

    created the configuration.php file and included it to the index.php file. Also created an uploades folder

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "";
-$dbname = "catalystit";
-
-//Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-//Check connection
-if($conn->connect_error){
-	die("Connection failed: " . $conn->connect_error);
-}
-
-//sql create table
-$sql = "CREATE TABLE users (
-name VARCHAR(100) NOT NULL,
-surname VARCHAR(100) NOT NULL,
-email VARCHAR(250) NOT NULL UNIQUE,
-)";
-
-if($conn->query($sql) === TRUE){
-  echo "Table users created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
+include "config.php"
 
 if(isset($_POST['but_import'])){
 	$target_dir = "uploads/";
