02101832695635c055249bcee665c8cf523a5cd3
Massimo <massimo.giangrande01@gmail.com>
2021-01-17 15:43:55 -0500

    In the config.php file, added a CHECK constraint when createing the user table. It makes sure that the emails are valid and all are lowercase. Also, both name and surename are checked if they are uppercase

--- config.php
+++ config.php
@@ -17,6 +17,8 @@ $sql = "CREATE TABLE users (
 name VARCHAR(100) NOT NULL,
 surname VARCHAR(100) NOT NULL,
 email VARCHAR(250) NOT NULL UNIQUE,
+CHECK (binary_checksum(email) = binary_checksum(lower(email)) and charindex('@', email) > 0),
+CHECK (binary_checksum(name) = binary_checksum(upper(name)) and binary_checksum(surname) = binary_checksum(upper(surname)))
 )";
 
 if($conn->query($sql) === TRUE){
